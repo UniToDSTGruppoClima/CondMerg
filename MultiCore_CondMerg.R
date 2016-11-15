e0ebe186ba653bb60c3c62793a32e49c7b44d56f
DiegoUniToDST <diego.guenzi@unito.it>
2016-11-15 16:48:48 +0100

    Update MultiCore_CondMerg.R

--- MultiCore_CondMerg.R
+++ MultiCore_CondMerg.R
@@ -5,8 +5,8 @@
 ###############################################################################
 ###############################################################################
 ###                                                                         ###
-### Copyright (C) 2016 - Diego Guenzi                                       ###
-###                                                                         ###
+### Copyright (C) 2016 - Diego Guenzi, Simona Fratianni, Rodolfo Boraso     ###
+###                      and Roberto Cremonini                              ###
 ###                                                                         ###
 ### This program is free software: you can redistribute it and/or modify    ###
 ### it under the terms of the GNU General Public License as published by    ###
